62ce885b3ca5ac1f1f85e52b0c06bc908a45fe50
Alan Caruso <alan.carusoo@gmail.com>
2022-10-27 14:17:48 -0300

    google maps implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('mapSearchField')
+  public searchField!: ElementRef;
+  @ViewChild(GoogleMap)
+  public map!: GoogleMap;
+
+
+
+
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    disableDefaultUI: true,
+    fullscreenControl: true,
+    disableDoubleClickZoom: true,
+    mapTypeId: 'hybrid',
+    // maxZoom:this.maxZoom,
+    // minZoom:this.minZoom,
+  };
+  latitude!: any;
+  longitude!: any;
+
   title = 'google-maps-app';
+  initialCoordinates = {
+    lat: -34.5982047,
+    lng: -58.4499887
+  };
+
+  mapConfigurations = {
+    fullscreenControl: true,
+    disableDefaultUI: true,
+    zoomControl: true
+  }
+
+  ngAfterViewInit(): void {
+    const searchBox = new google.maps.places.SearchBox(
+      this.searchField.nativeElement,
+    );
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+      this.searchField.nativeElement,
+    );
+
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+      if (places?.length === 0) {
+        return;
+      }
+
+      const bounds = new google.maps.LatLngBounds();
+      places?.forEach(place => {
+        if (!place.geometry || !place.geometry.location) {
+          return;
+        }
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport)
+        } else {
+          bounds.extend(place.geometry.location)
+        }
+      });
+      this.map.fitBounds(bounds)
+
+
+    })
+
+  }
+
+
+
 }
+
